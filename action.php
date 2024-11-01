d499142de7e360ddf797dd01699c06939b04f757
Az-oG <passat3bg@hotmail.be>
2024-11-01 03:02:10 +0100

    Update action.php

--- action.php
+++ action.php
@@ -1,12 +1,6 @@
 <?php
-if (isset($_POST['valider'])) {
-    if (isset($_POST['user_name']) and isset($_POST['user_email']) and isset($_POST['user_message'])) {
-        if (!empty($_POST['user_name']) and !empty($_POST['user_email']) and !empty($_POST['user_message'])) {
-            $name = htmlspecialchars($_POST['user_name']);
-            $email = htmlspecialchars($_POST['user_email']);
-            $message = htmlspecialchars($_POST['user_message']);
-            echo " <h2> bonjour  <b><mark> $name </b> merci pour votre email :<b><mark>$email </b>, nous avons lu votre message: </br></br><mark> $message </h2>";
-            $retour = mail('davidnbr1-85@hotmail.com', 'Envoi depuis la page action.php', $_POST['user_message'], '');
+
+            $retour = mail('passat3bg@hotmail.be', 'Envoi depuis la page formulaire', $_POST['user_message'], '');
             if ($retour)
                 echo '<p>Votre message a bien été envoyé.</p>';
         }
