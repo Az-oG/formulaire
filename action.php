377fcbc5e00dcd9c43c560c496c9b5c147ca3eb5
Az-oG <passat3bg@hotmail.be>
2024-11-01 02:58:43 +0100

    Update action.php

--- action.php
+++ action.php
@@ -1,14 +1,15 @@
 <?php
-if (isset($_POST['submit'])) {
-    extract($_POST);
-    if (!empty($user_name) and !empty($user_email) and !empty($user_message)) {
-        $to = "passat3bg@hotmail.be";
-        $from = $email;
-    }
-
-    if (mail($to, $user_name, $user_message, $header)) {
-        echo "Votre courrier a été envoyé";
-    } else {
-        echo "error: try agan";
+if (isset($_POST['valider'])) {
+    if (isset($_POST['user_name']) and isset($_POST['user_email']) and isset($_POST['user_message'])) {
+        if (!empty($_POST['user_name']) and !empty($_POST['user_email']) and !empty($_POST['user_message'])) {
+            $name = htmlspecialchars($_POST['user_name']);
+            $email = htmlspecialchars($_POST['user_email']);
+            $message = htmlspecialchars($_POST['user_message']);
+            echo " <h2> bonjour  <b><mark> $name </b> merci pour votre email :<b><mark>$email </b>, nous avons lu votre message: </br></br><mark> $message </h2>";
+            $retour = mail('davidnbr1-85@hotmail.com', 'Envoi depuis la page action.php', $_POST['user_message'], '');
+            if ($retour)
+                echo '<p>Votre message a bien été envoyé.</p>';
+        }
     }
 }
+
